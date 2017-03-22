df16b306013e8c091cb42e2339cfbd73d22c3b71
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2017-03-22 09:16:52 +0100

    Changes during review: Parasite vs. diversity and LRT testing.

--- R/3_Phyloseq_analyse_Hyena.R
+++ R/3_Phyloseq_analyse_Hyena.R
@@ -148,6 +148,42 @@ names(Phy.roles) <- c("Phylum", "role")
 
 tax.frame <- merge(tax.frame, Phy.roles, all.x=TRUE, by="Phylum")
 
+## here what we have in total RSVs
+as.data.frame(table(tax_table(PS)[, "Kingdom"]))
+
+RSVcounts <- as.data.frame(table(tax_table(PS)[, "Phylum"]))
+names(RSVcounts) <- c("Phylum", "numRSVs")
+
+RSVcounts$Phylum[RSVcounts$Phylum%in%"Vertebrata"] <-
+    "Chordata"
+
+RSVcounts$Phylum[RSVcounts$Phylum%in%"Chlorophyta_ph"] <-
+    "Chlorophyta"
+
+RSVcounts$Phylum[RSVcounts$Phylum%in%"Euglenida"] <-
+    "Euglenozoa"           
+
+RSVcounts$Phylum[RSVcounts$Phylum%in%"Spirochaetes"] <-
+    "Spirochaetae"         
+
+RSVcounts[RSVcounts$Phylum%in%euk_phyla& !RSVcounts$Phylum%in%spurious.phyla ,]
+
+## the rest are not assigned to phylum
+sum(RSVcounts[RSVcounts$Phylum%in%bak_phyla,"numRSVs"])
+sum(RSVcounts[RSVcounts$Phylum%in%euk_phyla,"numRSVs"])
+
+
+sum(otu_table(subset_taxa(PS, Kingdom %in% "Bacteria")))-
+sum(otu_table(subset_taxa(PS.genus, Kingdom %in% "Bacteria")))
+
+
+sum(otu_table(subset_taxa(PS, Kingdom %in% "Eukaryota")))-
+sum(otu_table(subset_taxa(PS.genus, Kingdom %in% "Eukaryota")))
+
+tax.frame.bac <- tax.frame[tax.frame$Kingdom%in%"Bacteria", ]
+tax.frame.euk <- tax.frame[tax.frame$Kingdom%in%"Eukaryota", ]
+
+
 devSVG("figures/figures_Hyena/Figure1a_taxon_dist.svg", width=12, height=6)
 ggplot(tax.frame, aes(x=Phylum, color=role, y=..count..)) +
     geom_bar() +
@@ -180,6 +216,8 @@ table1_Tax <- cbind(
 
 
 euk_phyla <- names(table(tax_table(subset_taxa(PS, Kingdom %in% "Eukaryota"))[, "Phylum"]))
+
+bac_phyla <- names(table(tax_table(subset_taxa(PS, Kingdom %in% "Bacteria"))[, "Phylum"]))
 
 undef <- c("", "uncultured","undef")
 clear.prey.phyla <- c("Vertebrata", "Chordata")
@@ -276,7 +314,6 @@ spear.cor <- data.frame(t(cor(All.data[, count.col], All.data[, tax.cols],
                               method="spearman",
                               use="pairwise.complete.obs")))
 
-
 top.cors.spear <- lapply(count.col, function (x){
         head(spear.cor[order(spear.cor[,x], decreasing=TRUE), x, drop=FALSE], n=10)
 })
@@ -284,16 +321,19 @@ top.cors.spear <- lapply(count.col, function (x){
 names(top.cors.spear) <- count.col
 
 ## Ancylostoma
-cor(All.data$Rhabditida, All.data$count_Ancylostoma_small,
+cor.test(All.data$Ancylostoma, All.data$count_Ancylostoma,
+         use="pairwise.complete.obs", method="spearman")
+
+cor.test(All.data$Rhabditida, All.data$count_Ancylostoma,
     use="pairwise.complete.obs", method="spearman")
 
-cor(All.data$Ancylostoma + All.data$Haemonchus,
-    All.data$count_Ancylostoma_small,
-    use="pairwise.complete.obs", method="spearman")
+cor.test(All.data$Ancylostoma + All.data$Haemonchus, ## + All.data$Ostertagia,
+         All.data$count_Ancylostoma,
+         use="pairwise.complete.obs", method="spearman")
 
-cor(All.data$Ancylostoma + All.data$Haemonchus + All.data$Ostertagia,
-    All.data$count_Ancylostoma_small,
-    use="pairwise.complete.obs", method="spearman")
+cor.test(All.data$Ancylostoma +  All.data$Ostertagia, 
+         All.data$count_Ancylostoma,
+         use="pairwise.complete.obs", method="spearman")
 
 devtools::source_gist("524eade46135f6348140",
                       filename = "ggplot_smooth_func.R")
@@ -303,7 +343,7 @@ ggplot(All.data, aes(Rhabditida+1 , count_Ancylostoma+1,
                      label=ID.Hyena)) +
     geom_point() +
     stat_smooth_func(geom="text", method="lm", hjust=0, parse=TRUE) +
-##    stat_smooth(method="lm", se=FALSE) +
+    stat_smooth(method="lm", se=FALSE) +
     scale_x_log10("Sequence abundance") +
     scale_y_log10("FEC") +
     ggtitle("Ancylostoma (Rhabditida)") +
@@ -331,11 +371,6 @@ cor.test(All.data$Spirometra, All.data$count_Spirometra,
 cor.test(All.data$Diphyllobothrium, All.data$count_Spirometra,
          use="pairwise.complete.obs", method="spearman")
 
-cor.test(All.data$Diphyllobothrium, All.data$count_Spirometra,
-         use="pairwise.complete.obs", method="spearman")
-
-
-
 pdf("figures/figures_Hyena/Figure2b_Spirometra.pdf")
 ggplot(All.data, aes(Diphyllobothriidea+1, count_Spirometra+1, label=ID.Hyena)) +
     geom_point() +
@@ -356,44 +391,25 @@ round(sort(Dip.counts/sum(Dip.counts)*100, decreasing=TRUE), 2)
 range(All.data[All.data$count_Spirometra==0 & All.data$Diphyllobothriidea>0,
                "Diphyllobothriidea"], na.rm=TRUE)
 
-
-## Cystoisospora misidentified as Eimeria
-cor.test(All.data$Cystoisospora, All.data$count_Cystoisospora,
-         use="pairwise.complete.obs", method="spearman")
-
-cor.test(All.data$Eimeria, All.data$count_Cystoisospora,
-         use="pairwise.complete.obs", method="spearman")
-
-## The two "Eimerias" help a little
-cor.test((All.data$Eimeria+ All.data$Eimeria.1), All.data$count_Cystoisospora,
-         use="pairwise.complete.obs", method="spearman")
-
 ## Best combinations for "_small"
-cor((All.data$Eimeria +
-     All.data$Besnoitia  +
-     All.data$Toxoplasma),
-    All.data$count_Cystoisospora_small,
-    use="pairwise.complete.obs", method="spearman")
 
-cor((All.data$Eimeriidae +
-     All.data$Besnoitia  +
-     All.data$Toxoplasma),
-    All.data$count_Cystoisospora,
-    use="pairwise.complete.obs", method="pearson")
-
-### Best combinations for "2"
-cor(All.data$Eimeria.1,
-    All.data$count_Cystoisospora2,
-    use="pairwise.complete.obs", method="spearman")
-
-### Best combinations for "2"
-cor(All.data$Eimeria.1,
-    All.data$count_Cystoisospora2,
-    use="pairwise.complete.obs", method="pearson")
-
-All.data$Cocci.comb <- All.data$Eimeriidae +
+All.data$Cocci.comb <-
+    All.data$Eimeriidae +
     All.data$Besnoitia +
     All.data$Toxoplasma
+
+cor.test((All.data$Coccidia),
+         All.data$count_Cystoisospora_small,
+         use="pairwise.complete.obs", method="spearman")
+
+cor.test((All.data$Cocci.comb),
+         All.data$count_Cystoisospora_small,
+         use="pairwise.complete.obs", method="spearman")
+
+### Best combinations for "2"
+cor.test(All.data$Eimeria.1,
+         All.data$count_Cystoisospora2,
+         use="pairwise.complete.obs", method="spearman")
 
 pdf("figures/figures_Hyena/Figure2c_Coccidia_small.pdf")
 ggplot(All.data, aes(Cocci.comb +1, 
@@ -409,9 +425,8 @@ dev.off()
 
 summary(lm(log10(count_Cystoisospora_small+1)~log10(Cocci.comb +1),
            data=All.data))
-                     
 
-pdf("figures/Coccidia_larger.pdf")
+pdf("figures/figures_Hyena/Figure2d_Coccidia_larger.pdf")
 ggplot(All.data, aes(Eimeria.1+1, count_Cystoisospora2+1,
                      label=ID.Hyena)) +
     geom_point()+
@@ -479,7 +494,16 @@ PS.rare.E <- subset_taxa(PS.rare,
                          !Phylum %in% c(clear.prey.phyla, undef) &
                          Kingdom %in% "Eukaryota")
 
+PS.r.g.E <- subset_taxa(PS.r.genus,
+                        !Phylum %in% c(clear.prey.phyla, undef) &
+                        Kingdom %in% "Eukaryota")
+
+
 do.call(rbind, test.Chao.rank(PS.rare.E))
+
+do.call(rbind, test.Chao.rank(PS.r.g.E))
+
+
 
 PS.rare.est <- estimate_richness(PS.rare.E, measures=c("Observed", "Chao1", "Shannon"))
 PS.rare.df <- merge(sample_data(PS.rare.E), PS.rare.est, by=0)
@@ -516,7 +540,7 @@ PS.r.g.df$pilou <- PS.r.g.df$Shannon/log(PS.r.g.df$Observed)
 PS.r.g.mdf = reshape2::melt(PS.r.g.df,
                              measure.vars = c("Observed", "Chao1", "pilou"))
 
-pdf("figures/figures_Hyena/Figure4b_euk_rank_diversity_Generas.pdf")
+pdf("figures/figures_Hyena/Figure4b_euk_rank_diversity_genera.pdf")
 ggplot(PS.r.g.mdf, aes(rank, value)) +
     geom_boxplot(na.rm = TRUE) +
     theme_bw()+
@@ -601,6 +625,15 @@ names(Chao.phyla)[chao.order]
 do.call(rbind, test.Chao.age(subset_taxa(PS.rare,
                                          Kingdom %in% "Bacteria"
                                          )))
+
+do.call(rbind, test.Chao.age(subset_taxa(PS.r.genus,
+                                         Kingdom %in% "Bacteria"
+                                         )))
+
+do.call(rbind, test.Chao.age(subset_taxa(PS.r.genus,
+                                         Kingdom %in% "Eukaryota"
+                                         )))
+
 
 PS.r.g.B <- subset_taxa(PS.r.genus,
                          Kingdom %in% "Bacteria")
@@ -726,9 +759,11 @@ logBAC <- transform_sample_counts(
 
 out.bc.log <- ordinate(logBAC, method = "NMDS", distance = "bray")
 
-devSVG("figures/figures_Hyena/Figure3b_Ord_bac.svg")
-plot_ordination(logBAC, out.bc.log, color = "age") +
+pdf("figures/figures_Hyena/Figure3b_Ord_bac.pdf")
+plot_ordination(logBAC, out.bc.log, color="age", shape="age") +
     labs(col = "Binned Age") +
+    scale_shape_discrete(solid=FALSE) +
+    guides(col = guide_legend(override.aes = list(size = 3))) +
     theme_bw() +
     ggtitle("Ordination on log 1+x transformed abundance of bacterial genera")
 dev.off()
@@ -750,10 +785,11 @@ pls_biplot_Bac$scores <- data.frame(sample_data(logBAC),
                                     pls_biplot_Bac$scores)
 
 tax <- data.frame(tax_table(logBAC), stringsAsFactors = FALSE)
-main_phyla <- c("Tenericutes", "Actinobacteria","Bacteroidetes", "Firmicutes")
+main_phyla <- sort(c("Tenericutes", "Actinobacteria",
+                     "Bacteroidetes", "Firmicutes", "Proteobacteria"))
 
-tax$Phylum[!(tax$Phylum %in% main_phyla)] <- "Other"
-tax$Order <- factor(tax$Order, levels = c(main_phyla, "Other"))
+tax$Phylum[!(tax$Phylum %in% main_phyla)] <- "Z_Other"
+tax$Phylum <- factor(tax$Phylum, levels = c(main_phyla, "Z_Other"))
 class(pls_biplot_Bac$loadings) <- "matrix"
 pls_biplot_Bac$loadings <- data.frame(tax, pls_biplot_Bac$loadings)
 
@@ -761,10 +797,10 @@ pdf("figures/figures_Hyena/Figure3c_pls_bi_bac.pdf")
 ggplot() +
     geom_point(data = pls_biplot_Bac$loadings,
                aes(x = 25 * Comp.1, y = 25 * Comp.2, col = Phylum),
-               size = 1) +
+               size = 2) +
     geom_point(data = pls_biplot_Bac$scores,
                aes(x = Comp.1, y = Comp.2, shape = age), size = 3) +
-    scale_color_brewer(palette = "Set2") +
+    scale_color_brewer(palette = "Set1") +
     scale_shape_discrete(solid=FALSE) +
     labs(x = "Axis1", y = "Axis2", col = "Phylum") +
     guides(col = guide_legend(override.aes = list(size = 3))) +
@@ -773,12 +809,43 @@ ggplot() +
 dev.off()
 
 
+
+scores.vector <- (pls_biplot_Bac$loadings$Comp.1*-1) *
+    pls_biplot_Bac$loadings$Comp.2 
+
+pls.ordered <- pls_biplot_Bac$loadings[order(scores.vector), ]
+
+q25 <- head(pls.ordered, n = (nrow(pls.ordered)*0.25))
+q75 <- tail(pls.ordered, n = (nrow(pls.ordered)*0.25))
+
+tar.Phylum <- "Tenericutes"
+
+fisher.test(pls.ordered$Genus %in%
+            pls.ordered$Genus[pls.ordered$Phylum%in%tar.Phylum],
+            pls.ordered$Genus %in%
+            q75$Genus)
+
+tar.Phylum <- "Bacteroidetes"
+
+fisher.test(pls.ordered$Genus %in%
+            pls.ordered$Genus[pls.ordered$Phylum%in%tar.Phylum],
+            pls.ordered$Genus %in%
+            q75$Genus, alternative="greater")
+
+
+tar.Phylum <- "Actinobacteria"
+
+fisher.test(pls.ordered$Genus %in%
+            pls.ordered$Genus[pls.ordered$Phylum%in%tar.Phylum],
+            pls.ordered$Genus %in%
+            q25$Genus)
+
+
 ## get unnormalized data to do the DESeq stuff
 
 load(file="/SAN/Metabarcoding/phlyoSeq_Hy_raw.Rdata") ## -> PS.raw
 
 PS.raw.genus <- tax_glom(PS.raw, "Genus")
-
 
 ## differences by phyla
 library(DESeq2)
@@ -795,17 +862,16 @@ Bac.sigtab
 library(scales)
 
 get.sigtab.plot <- function (sigtab){
-    x <- tapply(sigtab$log2FoldChange, sigtab$Phylum, function(x) max(x))
-    x <- sort(x, TRUE)
-    sigtab$Phylum <- factor(as.character(sigtab$Phylum), levels=names(x))
-                                        # Genus order
+    sigtab$Phylum <- factor(as.character(sigtab$Phylum), levels=main_phyla)
+    ## Genus order
     x <- tapply(sigtab$log2FoldChange, sigtab$Genus, function(x) max(x))
     x <- sort(x, TRUE)
     sigtab$Genus <- factor(as.character(sigtab$Genus), levels=names(x))
     ggplot(sigtab, aes(x=Genus, y=log2FoldChange, color=Phylum, label=scientific(padj))) +
         geom_point(size=6) +
-        geom_text(vjust=1.6)+
+        geom_text(vjust=1.6, color="black")+
         theme_bw()+
+        scale_color_brewer(palette = "Set1") +
         theme(axis.text.x = element_text(angle = -45, hjust = 0, vjust=1))
 }
 
@@ -815,6 +881,9 @@ devSVG("figures/figures_Hyena/Figure3d_single_diff.svg")
 plot.bacterial.diff
 dev.off()
 
+pdf("figures/figures_Hyena/Figure3d_single_diff.pdf")
+plot.bacterial.diff
+dev.off()
 
 ######################### Eukaryotes ############################
 
@@ -841,13 +910,13 @@ pls_biplot_Euk$scores <- data.frame(sample_data(logEuk),
 
 tax <- data.frame(tax_table(logEuk), stringsAsFactors = FALSE)
 
-main_phyla <- c("Basidiomycota", "Ascomycota", "Blastocladiomycota",
-                "Apicomplexa", "Ochrophyta", "Cercozoa", "Ciliophora",
-                "Nematoda")
+main_phyla <- sort(c("Basidiomycota", "Ascomycota", "Platyhelminthes",
+                     "Arthropoda", "Apicomplexa", "Ochrophyta",
+                     "Blastocladiomycota", "Nematoda"))
 
-tax$Phylum[!(tax$Phylum %in% main_phyla)] <- "Other"
+tax$Phylum[!(tax$Phylum %in% main_phyla)] <- "Z_Other"
 
-tax$Phylum <- factor(tax$Phylum, levels = c(main_phyla, "Other"))
+tax$Phylum <- factor(tax$Phylum, levels = c(main_phyla, "Z_Other"))
 
 class(pls_biplot_Euk$loadings) <- "matrix"
 pls_biplot_Euk$loadings <- merge(tax, pls_biplot_Euk$loadings, by=0)
@@ -858,7 +927,7 @@ colnames(pls_biplot_Euk$loadings) <-
 pdf("figures/figures_Hyena/Figure4c_pls_bi_euk.pdf", width=5, height=10)
 ggplot() +
     geom_jitter(data = pls_biplot_Euk$loadings,
-                aes(x="1", y = 25 * Comp.1 , col = Phylum), size = 1) +
+                aes(x="1", y = 25 * Comp.1 , col = Phylum), size = 2) +
     geom_jitter(data = pls_biplot_Euk$scores,
                aes(x="1",y = Comp.1, shape=rank), size=4) +
     scale_color_brewer(palette = "Set1") +
@@ -868,6 +937,33 @@ ggplot() +
     ##    theme(panel.border = element_rect(color = "#787878", fill = alpha("white", 0)))
     theme_bw()
 dev.off()
+
+
+pls.ordered <- pls_biplot_Euk$loadings[order(pls_biplot_Euk$loadings$Comp.1), ]
+
+q25 <- head(pls.ordered, n = (nrow(pls.ordered)*0.25))
+q75 <- tail(pls.ordered, n = (nrow(pls.ordered)*0.25))
+
+
+tar.Phylum <- "Basidiomycota"
+
+fisher.test(pls.ordered$Genus %in%
+            pls.ordered$Genus[pls.ordered$Phylum%in%tar.Phylum],
+            pls.ordered$Genus %in%
+            q75$Genus)
+
+
+tar.Phylum <- "Apicomplexa"
+
+fisher.test(pls.ordered$Genus %in%
+            pls.ordered$Genus[pls.ordered$Phylum%in%tar.Phylum],
+            pls.ordered$Genus %in%
+            q25$Genus)
+
+fisher.test(pls.ordered$Genus %in%
+            pls.ordered$Genus[pls.ordered$Phylum%in%tar.Phylum],
+            pls.ordered$Genus %in%
+            c(q25$Genus, q75$Genus))
 
 
 ## Eukaryote single genus differences
@@ -889,3 +985,13 @@ plot.euk.diff <- get.sigtab.plot(Euk.sigtab)
 devSVG("figures/figures_Hyena/Figure4d_single_diff.svg")
 plot.euk.diff
 dev.off()
+
+
+pdf("figures/figures_Hyena/Figure4d_single_diff.pdf")
+plot.euk.diff
+dev.off()
+
+
+NSplsFitEuk.age <- train(age ~ ., data = dataMatrixEuk,
+                         method = "pls", preProc= "center",
+                         trControl = trainControl(method = "LOOCV"))
